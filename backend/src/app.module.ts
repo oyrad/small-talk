87a13e28d6ee83ab65b5e9b7ff1cabe19c427369
Dario Susanj <dario.susanj@tradelocker.com>
2025-03-18 08:32:04 +0100

    add delete empty rooms cron job

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './message/message.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
@@ -17,10 +18,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         type: 'postgres',
         url: configService.get<string>('DATABASE_URL'),
         autoLoadEntities: true,
-        synchronize: true,
+        synchronize: configService.get<string>('NODE_ENV') === 'development',
       }),
       inject: [ConfigService],
     }),
+    ScheduleModule.forRoot(),
     RoomModule,
     UserModule,
     MessageModule,
---
+++ backend/src/tasks/tasks.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Room } from '../room/room.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TasksService {
+  private readonly logger = new Logger(TasksService.name);
+
+  constructor(
+    @InjectRepository(Room)
+    private readonly roomRepository: Repository<Room>,
+  ) {}
+
+  @Cron(CronExpression.EVERY_WEEK, {
+    name: 'delete-empty-rooms',
+    timeZone: 'Europe/Zagreb',
+  })
+  async deleteEmptyRooms() {
+    this.logger.log(`Deleting empty rooms`);
+    const rooms = await this.roomRepository
+      .createQueryBuilder('room')
+      .leftJoinAndSelect('room.users', 'users')
+      .leftJoinAndSelect('room.messages', 'messages')
+      .groupBy('room.id')
+      .having('COUNT(users.id) = 1')
+      .andHaving('COUNT(messages.id) = 0')
+      .select('room.id')
+      .getMany();
+
+    if (rooms.length > 0) {
+      const roomIds = rooms.map((room) => room.id);
+      this.logger.log(`Deleting rooms with IDs: ${roomIds.join(', ')}`);
+      await this.roomRepository.delete(roomIds);
+    }
+  }
+}
