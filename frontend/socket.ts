9ebc08367a9f36ac19305c77cec5d0bbd5962d2b
oyrad <dario.susanj2@gmail.com>
2025-03-18 19:03:31 +0100

    fix socket url

--- frontend/socket.ts
+++ frontend/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 import { ClientToServerEvents, ServerToClientEvents } from '@/types/socket-events';
 
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(process.env.NEXT_PUBLIC_API_URL, {
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
   transports: ['websocket'],
 });
